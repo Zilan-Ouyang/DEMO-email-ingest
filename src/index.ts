299131058da11dae4b4f442e3da61a894025a4ab
haydenwies <haydenwies@gmail.com>
2024-08-12 11:27:04 -0400

    First commit.

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from "express"
+import multer from "multer"
+import fs from "fs"
+
+const app = express()
+const upload = multer()
+
+app.post("/upload", upload.any(), (req: Request, res: Response) => {
+	if (!req.file) {
+		return res.status(400).send("No file uploaded.")
+	}
+
+	const files = req.files
+	console.log(files)
+
+	// const results: (string | Buffer)[] = []
+	// const readStream = fs.createReadStream(req.file.path)
+
+	// readStream.on("open", () => {
+	// 	console.log("PDF file stream opened successfully")
+	// })
+
+	// readStream.on("data", (chunk) => {
+	// 	console.log("Reading chunk:", chunk)
+	// 	results.push(chunk)
+	// })
+
+	// readStream.on("end", () => {
+	// 	console.log("Finished reading the PDF file")
+	// 	console.log(results)
+
+	// 	res.status(200).send("Ok")
+	// })
+
+	// readStream.on("error", (err) => {
+	// 	console.error("An error occurred while reading the PDF file:", err)
+
+	// 	res.status(500).send(err.message)
+	// })
+
+	res.status(200).send()
+})
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000")
+})
