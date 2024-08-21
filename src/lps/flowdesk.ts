e8c674b3cdf9b50bdf150156ea11d4f75c48fcaf
haydenwies <haydenwies@gmail.com>
2024-08-21 12:47:44 -0400

    Formatting values for flowdesk.

--- src/lps/flowdesk.ts
+++ src/lps/flowdesk.ts
@@ -7,16 +7,17 @@ export const parseFlowdeskTransactionReport = async (
 	if (!Array.isArray(files)) throw new Error("Files not in array format")
 
 	const fileArr = await readPdf(files[0])
+	console.log(fileArr)
 
 	const [m, d, y] = fileArr[3].split("/")
 
 	const date = String(Math.floor(new Date(`${y}-${m}-${d}T00:00:00Z`).getTime() / 1000))
 	const coin = fileArr[13]
 	const side = fileArr[15]
-	const quantity = fileArr[17]
-	const price = fileArr[19]
+	const quantity = parseFloat(fileArr[17].replace(/,/g, "")).toString()
+	const price = parseFloat(fileArr[19].replace(/,/g, "")).toString()
 	const settlementCurrency = fileArr[21]
-	const notional = fileArr[23]
+	const notional = parseFloat(fileArr[23].replace(/,/g, "")).toString()
 
 	const report: string[][] = []
 	report.push(
